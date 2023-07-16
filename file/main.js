ba4b7e9773c29368edec389f657b4610406aa1dc
fahad <sfahadali894@gmail.com>
2023-07-16 04:10:32 -0700

    all repo commit in new proj insert

---
+++ file/main.js
@@ -0,0 +1,8 @@
+// function calculate() {
+//   var input1 = parseFloat(document.getElementById("input1").value);
+//   var input2 = parseFloat(document.getElementById("input2").value);
+  
+//   var sum = input1 + input2;
+  
+//   document.getElementById("result").innerHTML = "Sum: " + sum;
+// }
